0226d678bec37a75358ca1efffc7471e365f0277
xordog <xordog@gmail.com>
2020-01-18 01:38:30 +0900

    background() accepts transparent color.

--- lib/rubysketch/processing.rb
+++ lib/rubysketch/processing.rb
@@ -598,7 +598,16 @@ module RubySketch
     # @return [nil] nil
     #
     def background (*args)
-      @painter__.background(*to_rgba__(*args))
+      rgba = to_rgba__ *args
+      if rgba[3] == 1
+        @painter__.background *rgba
+      else
+        f = @painter__.fill *rgba
+        s = @painter__.stroke nil
+        @painter__.rect 0, 0, width, height
+        @painter__.fill   f
+        @painter__.stroke s
+      end
       nil
     end
 
