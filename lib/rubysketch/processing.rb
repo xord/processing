5f8a9a010ebe5ebb21d6556fd482da0dbb3725ef
xordog <xordog@gmail.com>
2020-02-19 23:27:58 +0900

    format code.

--- lib/rubysketch/processing.rb
+++ lib/rubysketch/processing.rb
@@ -1000,72 +1000,74 @@ module RubySketch
       Rays.perlin(x, y) / 2.0 + 1.0
     end
 
-    # Font object.
-    #
-    class Font
-
-      # Initialize font.
-      #
-      # @private
-      #
-      def initialize (font)
-        @font = font
-      end
-
-      # Returns bounding box.
-      #
-      # @overload textBounds(str)
-      # @overload textBounds(str, x, y)
-      # @overload textBounds(str, x, y, fontSize)
-      #
-      # @param str      [String]  text to calculate bounding box
-      # @param x        [Numeric] horizontal position of bounding box
-      # @param y        [Numeric] vertical position of bounding box
-      # @param fontSize [Numeric] font size
-      #
-      # @return [TextBounds] bounding box for text
-      #
-      def textBounds (str, x = 0, y = 0, fontSize = nil)
-        f = fontSize ? Rays::Font.new(@font.name, fontSize) : @font
-        TextBounds.new x, y, x + f.width(str), y + f.height
-      end
-
-      # Bounding box for text.
-      #
-      class TextBounds
-
-        # Horizontal position
-        #
-        attr_reader :x
-
-        # Vertical position
-        #
-        attr_reader :y
-
-        # Width of bounding box
-        #
-        attr_reader :w
-
-        # Height of bounding box
-        #
-        attr_reader :h
-
-        # Initialize bouding box.
-        #
-        # @param x [Numeric] horizontal position
-        # @param y [Numeric] vertical position
-        # @param w [Numeric] width of bounding box
-        # @param h [Numeric] height of bounding box
-        #
-        def initialize (x, y, w, h)
-          @x, @y, @w, @h = x, y, w, h
-        end
-
-      end# TextBounds
-
-    end# Font
-
   end# Processing
+
+
+  # Font object.
+  #
+  class Processing::Font
+
+    # Initialize font.
+    #
+    # @private
+    #
+    def initialize (font)
+      @font = font
+    end
+
+    # Returns bounding box.
+    #
+    # @overload textBounds(str)
+    # @overload textBounds(str, x, y)
+    # @overload textBounds(str, x, y, fontSize)
+    #
+    # @param str      [String]  text to calculate bounding box
+    # @param x        [Numeric] horizontal position of bounding box
+    # @param y        [Numeric] vertical position of bounding box
+    # @param fontSize [Numeric] font size
+    #
+    # @return [TextBounds] bounding box for text
+    #
+    def textBounds (str, x = 0, y = 0, fontSize = nil)
+      f = fontSize ? Rays::Font.new(@font.name, fontSize) : @font
+      TextBounds.new x, y, x + f.width(str), y + f.height
+    end
+
+  end# Processing::Font
+
+
+  # Bounding box for text.
+  #
+  class Processing::TextBounds
+
+    # Horizontal position
+    #
+    attr_reader :x
+
+    # Vertical position
+    #
+    attr_reader :y
+
+    # Width of bounding box
+    #
+    attr_reader :w
+
+    # Height of bounding box
+    #
+    attr_reader :h
+
+    # Initialize bouding box.
+    #
+    # @param x [Numeric] horizontal position
+    # @param y [Numeric] vertical position
+    # @param w [Numeric] width of bounding box
+    # @param h [Numeric] height of bounding box
+    #
+    def initialize (x, y, w, h)
+      @x, @y, @w, @h = x, y, w, h
+    end
+
+  end# Processing::TextBounds
 
 
 end# RubySketch
