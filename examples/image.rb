efa61ec32ac8f6bade07d2852712d57071fe63f2
xordog <xordog@gmail.com>
2020-03-26 23:21:23 +0900

    add examples/image.rb

---
+++ examples/image.rb
@@ -0,0 +1,13 @@
+%w[xot rays reflex rubysketch]
+  .map  {|s| File.expand_path "../../#{s}/lib", __dir__}
+  .each {|s| $:.unshift s if !$:.include?(s) && File.directory?(s)}
+
+require 'rubysketch-processing'
+
+
+icon = loadImage 'rubysketch.png'
+
+draw do
+  background 0, 10
+  image icon, mouseX, mouseY, icon.width / 10, icon.height / 10
+end
