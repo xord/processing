f6157840c36cacecca6e1428ddf08a4e1b23a2a2
xordog <xordog@gmail.com>
2020-11-18 10:18:09 +0900

    add examples/camera.rb

---
+++ examples/camera.rb
@@ -0,0 +1,14 @@
+%w[xot rays reflex rubysketch]
+  .map  {|s| File.expand_path "../../#{s}/lib", __dir__}
+  .each {|s| $:.unshift s if !$:.include?(s) && File.directory?(s)}
+
+require 'rubysketch-processing'
+
+
+cam = Capture.new
+cam.start
+
+draw do
+  background 0
+  image cam, 0, 0
+end
