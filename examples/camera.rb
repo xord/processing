16cfe18e06f399d2d10e00a7bf046dfd5fc930d7
xordog <xordog@gmail.com>
2020-11-29 22:48:17 +0900

    revert camera.rb

--- examples/camera.rb
+++ examples/camera.rb
@@ -5,29 +5,10 @@
 require 'rubysketch-processing'
 
 
-w, h = width, height
-
-cam = Capture.new w, h, Capture.list.last
+cam = Capture.new 300, 300
 cam.start
 
-images = 60.times.map {
-  Graphics.new w, h
-}
-
 draw do
-  if frameCount % 2 == 0
-    images.unshift images.pop
-    images.first.tap do |image|
-      image.beginDraw {
-        image.image cam, 0, 0
-      }
-    end
-  end
-
   background 0
-  segment_h= h / images.size
-  images.each.with_index do |image, i|
-    y = i * segment_h
-    copy image, 0, y, w, segment_h, 0, y, w, segment_h
-  end
+  image cam, 0, 0
 end
