09b281b39197c22aaa4ac8f9978291682f75cd93
xordog <xordog@gmail.com>
2022-11-12 01:42:16 +0900

    move utility functions to the GraphicsContext

--- test/processing/test_utility.rb
+++ test/processing/test_utility.rb
@@ -6,29 +6,36 @@ require_relative 'helper'
 
 class TestProcessingUtility < Test::Unit::TestCase
 
-  def context()
-    RubySketch::Processing::Context.new RubySketch::Window.new
-  end
+  P = RubySketch::Processing
+
+  include P::GraphicsContext
 
   def test_random()
-    c = context
+    assert_equal Float,  random(1).class
+    assert_equal Float,  random(1.0).class
+    assert_equal Symbol, random((:a..:z).to_a).class
 
-    assert_equal Float,  c.random(1).class
-    assert_equal Float,  c.random(1.0).class
-    assert_equal Symbol, c.random((:a..:z).to_a).class
-
-    assert_not_equal c.random, c.random
+    assert_not_equal random, random
 
     10000.times do
-      n = c.random
+      n = random
       assert 0 <= n && n < 1
 
-      n = c.random 1
+      n = random 1
       assert 0 <= n && n < 1
 
-      n = c.random 1, 2
+      n = random 1, 2
       assert 1.0 <= n && n < 2.0
     end
+  end
+
+  def test_createVector()
+    assert_equal P::Vector, createVector(1, 2).class
+  end
+
+  def test_createShader()
+    fs = "void main() {gl_FragColor = vec4(1.0);}"
+    assert_equal P::Shader, createShader(nil, fs).class
   end
 
 end# TestProcessingUtility
