73b6cce721fd5fa3833512b94787a8e30881e4b6
xordog <xordog@gmail.com>
2023-01-30 00:47:51 +0900

    define constants outside of refinements

--- lib/processing.rb
+++ lib/processing.rb
@@ -3,24 +3,25 @@ require 'processing/all'
 
 module Processing
   WINDOW  = Processing::Window.new {start}
-  context = Processing::Context.new WINDOW
+  CONTEXT = Processing::Context.new WINDOW
 
   refine Object do
-    (context.methods - Object.instance_methods).each do |method|
+    (CONTEXT.methods - Object.instance_methods).each do |method|
       define_method method do |*args, **kwargs, &block|
-        context.__send__ method, *args, **kwargs, &block
+        CONTEXT.__send__ method, *args, **kwargs, &block
       end
-    end
-
-    context.class.constants.each do |const|
-      self.class.const_set const, context.class.const_get(const)
     end
   end
 end# Processing
 
 
 begin
-  w = Processing::WINDOW
+  w, c = Processing::WINDOW, Processing::CONTEXT
+
+  c.class.constants.each do |const|
+    self.class.const_set const, c.class.const_get(const)
+  end
+
   w.__send__ :begin_draw
   at_exit do
     w.__send__ :end_draw
