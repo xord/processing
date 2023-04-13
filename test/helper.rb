a8adfbb9265b206e9a07719155328c82eda00c6c
xordog <xordog@gmail.com>
2023-04-14 02:42:22 +0900

    require 'test/unit' at last for the order of 'at_exit'

--- test/helper.rb
+++ test/helper.rb
@@ -5,9 +5,10 @@
   .map  {|s| File.expand_path "../#{s}/lib", __dir__}
   .each {|s| $:.unshift s if !$:.include?(s) && File.directory?(s)}
 
-require 'test/unit'
 require 'xot/test'
 require 'processing/all'
+
+require 'test/unit'
 
 include Xot::Test
 
