5c9a2701fce3f3b050692773859ab34da035d8af
xordog <xordog@gmail.com>
2023-11-09 00:30:11 +0900

    refine tests

--- test/draw/test_draw.rb
+++ test/draw/test_draw.rb
@@ -4,19 +4,19 @@ require_relative 'helper'
 class TestDraw < Test::Unit::TestCase
 
   def test_rect()
-    assert_draw 100, 100,                   'rect 10, 10, 50, 50'
-    assert_draw 100, 100, 'rectMode CORNER;  rect 10, 10, 50, 50'
-    assert_draw 100, 100, 'rectMode CORNERS; rect 10, 10, 50, 50'
-    assert_draw 100, 100, 'rectMode CENTER;  rect 10, 10, 50, 50'
-    assert_draw 100, 100, 'rectMode RADIUS;  rect 10, 10, 50, 50'
+    assert_draw                   'rect 10, 20, 30, 40'
+    assert_draw 'rectMode CORNER;  rect 10, 20, 30, 40'
+    assert_draw 'rectMode CORNERS; rect 10, 20, 30, 50'
+    assert_draw 'rectMode CENTER;  rect 40, 50, 30, 40'
+    assert_draw 'rectMode RADIUS;  rect 40, 50, 30, 40'
   end
 
   def test_ellipse()
-    assert_draw 100, 100,                      'ellipse 10, 10, 50, 50', threshold: 0.98
-    assert_draw 100, 100, 'ellipseMode CORNER;  ellipse 10, 10, 50, 50', threshold: 0.98
-    assert_draw 100, 100, 'ellipseMode CORNERS; ellipse 10, 10, 50, 50', threshold: 0.98
-    assert_draw 100, 100, 'ellipseMode CENTER;  ellipse 10, 10, 50, 50', threshold: 0.98
-    assert_draw 100, 100, 'ellipseMode RADIUS;  ellipse 10, 10, 50, 50', threshold: 0.98
+    assert_draw                      'ellipse 40, 50, 30, 40', threshold: 0.98
+    assert_draw 'ellipseMode CORNER;  ellipse 10, 20, 30, 40', threshold: 0.98
+    assert_draw 'ellipseMode CORNERS; ellipse 10, 20, 30, 50', threshold: 0.98
+    assert_draw 'ellipseMode CENTER;  ellipse 40, 50, 30, 40', threshold: 0.98
+    assert_draw 'ellipseMode RADIUS;  ellipse 40, 50, 30, 40', threshold: 0.98
   end
 
 end# TestDraw
