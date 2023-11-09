79293a3d1e5a69aa9a7390b94d48db1f1dc0c450
xordog <xordog@gmail.com>
2023-11-09 10:24:42 +0900

    use default threshold 0.98

--- test/draw/test_draw.rb
+++ test/draw/test_draw.rb
@@ -12,11 +12,11 @@ class TestDraw < Test::Unit::TestCase
   end
 
   def test_ellipse()
-    assert_draw                      'ellipse 40, 50, 30, 40', threshold: 0.98
-    assert_draw 'ellipseMode CORNER;  ellipse 10, 20, 30, 40', threshold: 0.98
-    assert_draw 'ellipseMode CORNERS; ellipse 10, 20, 30, 50', threshold: 0.98
-    assert_draw 'ellipseMode CENTER;  ellipse 40, 50, 30, 40', threshold: 0.98
-    assert_draw 'ellipseMode RADIUS;  ellipse 40, 50, 30, 40', threshold: 0.98
+    assert_draw                      'ellipse 40, 50, 30, 40'
+    assert_draw 'ellipseMode CORNER;  ellipse 10, 20, 30, 40'
+    assert_draw 'ellipseMode CORNERS; ellipse 10, 20, 30, 50'
+    assert_draw 'ellipseMode CENTER;  ellipse 40, 50, 30, 40'
+    assert_draw 'ellipseMode RADIUS;  ellipse 40, 50, 30, 40'
   end
 
 end# TestDraw
