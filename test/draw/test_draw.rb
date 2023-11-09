3316dc796698da2d12ae1a4998413d3726993d19
xordog <xordog@gmail.com>
2023-11-09 10:26:47 +0900

    Add draw tests for default colors

--- test/draw/test_draw.rb
+++ test/draw/test_draw.rb
@@ -3,6 +3,29 @@ require_relative 'helper'
 
 class TestDraw < Test::Unit::TestCase
 
+  THRESHOLD_TO_BE_FIXED = 0.0
+
+  def test_default_background_color()
+    assert_draw '', draw_header: '', threshold: THRESHOLD_TO_BE_FIXED
+  end
+
+  def test_default_fill_color()
+    assert_draw <<~END, draw_header: ''
+      background 100
+      noStroke
+      rect 10, 10, 50, 50
+    END
+  end
+
+  def test_default_stroke_color()
+    assert_draw <<~END, draw_header: ''
+      background 100
+      noFill
+      strokeWeight 10
+      line 10, 10, 50, 50
+    END
+  end
+
   def test_rect()
     assert_draw                   'rect 10, 20, 30, 40'
     assert_draw 'rectMode CORNER;  rect 10, 20, 30, 40'
