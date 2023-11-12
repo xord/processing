89bd4be7050bc2690200b38950d5894470fc57ef
xordog <xordog@gmail.com>
2023-11-13 01:35:58 +0900

    Add empty Shape class

---
+++ lib/processing/shape.rb
@@ -0,0 +1,34 @@
+module Processing
+
+
+  # Shape object.
+  #
+  class Shape
+
+    def initialize()
+    end
+
+    def width = nil
+    def height = nil
+    def isVisible = nil
+    def setVisible = nil
+    def beginShape = nil
+    def endShape = nil
+    def getChildCount = nil
+    def getChild = nil
+    def addChild = nil
+    def getVertexCount = nil
+    def getVertex = nil
+    def setVertex = nil
+    def translate = nil
+    def rotateX = nil
+    def rotateY = nil
+    def rotateZ = nil
+    def rotate = nil
+    def scale = nil
+    def resetMatrix = nil
+
+  end# Shape
+
+
+end# Processing
