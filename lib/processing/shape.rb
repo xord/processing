469917dd808985f68b81fe105216ea79222daad5
xordog <xordog@gmail.com>
2023-11-15 01:45:27 +0900

    Add method docs

--- lib/processing/shape.rb
+++ lib/processing/shape.rb
@@ -5,29 +5,47 @@ module Processing
   #
   class Shape
 
+    # @private
     def initialize(polygon = nil)
       @polygon = polygon
       @visible = true
     end
 
+    # Gets width of shape.
+    #
+    # @return [Numeric] width of shape
+    #
     def width()
       return 0 unless @polygon
       (@bounds ||= @polygon.bounds).width
     end
 
+    # Gets height of shape.
+    #
+    # @return [Numeric] height of shape
+    #
     def height()
       return 0 unless @polygon
       (@bounds ||= @polygon.bounds).height
     end
 
+    # Returns whether the shape is visible or not.
+    #
+    # @return [Boolean] visible or not
+    #
     def isVisible()
       @visible
     end
 
     alias visible? isVisible
 
+    # Sets whether to display the shape or not.
+    #
+    # @return [nil] nil
+    #
     def setVisible(visible)
       @visible = !!visible
+      nil
     end
 
     def beginShape = nil
