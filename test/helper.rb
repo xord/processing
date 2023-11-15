656cb0045f1a1bb876be118cf28b95817f1f5acb
xordog <xordog@gmail.com>
2023-11-16 02:33:32 +0900

    move test/draw/* files to test/

--- test/helper.rb
+++ test/helper.rb
@@ -5,11 +5,32 @@
 require 'xot/test'
 require 'processing/all'
 
+require 'digest/md5'
+require 'fileutils'
 require 'tempfile'
 require 'test/unit'
 
+require_relative 'p5'
+
 include Xot::Test
 
+
+DEFAULT_HEADER = <<~END
+  background 100
+  fill 255, 0, 0
+  stroke 0, 255, 0
+  strokeWeight 50
+END
+
+
+def md5(s)
+  Digest::MD5.hexdigest s
+end
+
+def mkdir(dir: nil, filename: nil)
+  path = dir || File.dirname(filename)
+  FileUtils.mkdir_p path unless File.exist? path
+end
 
 def temppath(ext: nil, &block)
   f     = Tempfile.new
@@ -18,6 +39,16 @@ def temppath(ext: nil, &block)
   f.close!
   block.call path
   File.delete path
+end
+
+def test_output_path(label, source = nil, ext: '.png', dir: ext)
+  path = File.join __dir__, dir, label + (source ? "_#{md5 source}" : '') + ext
+  mkdir filename: path
+  path
+end
+
+def test_label(index = 1)
+  caller_locations[index].then {|loc| "#{loc.label}_#{loc.lineno}"}
 end
 
 def get_pixels(image)
@@ -35,6 +66,15 @@ def graphics(width = 10, height = 10, *args, &block)
   end
 end
 
+def test_draw(*sources, width: 1000, height: 1000, pixelDensity: 1, label: nil)
+  graphics(width, height, pixelDensity).tap do |g|
+    src = sources.compact.join "\n"
+    g.beginDraw {g.instance_eval src}
+    g.save test_output_path(label, src) if label
+  end
+end
+
+
 def assert_equal_vector(v1, v2, delta = 0.000001)
   assert_in_delta v1.x, v2.x, delta
   assert_in_delta v1.y, v2.y, delta
@@ -51,4 +91,40 @@ def assert_equal_pixels(expected, actual, threshold: 1.0)
   assert equal_rate >= threshold, <<~EOS
     The rate of the same pixel #{equal_rate} is below the threshold #{threshold}
   EOS
+end
+
+def assert_draw_equal(
+  *shared_header, expected, actual, default_header: DEFAULT_HEADER,
+  width: 1000, height: 1000, threshold: 1.0, label: test_label)
+
+  e = test_draw default_header, *shared_header, expected, label: "#{label}_expected"
+  a = test_draw default_header, *shared_header, actual,   label: "#{label}_actual"
+
+  assert_equal_pixels e, a, threshold: threshold
+end
+
+def assert_draw(
+  *sources, default_header: DEFAULT_HEADER,
+  width: 1000, height: 1000, threshold: 0.99, label: test_label)
+
+  source = [default_header, *sources].compact.join("\n")
+  path   = test_output_path "#{label}_expected", source
+
+  pd     = draw_p5rb width, height, source, path, headless: true
+  actual = test_draw source, width: width, height: height, pixelDensity: pd
+  actual.save path.sub('_expected', '_actual')
+
+  assert_equal_pixels actual.loadImage(path), actual, threshold: threshold
+end
+
+def assert_fill(*sources, **kwargs)
+  assert_draw 'noStroke', *sources, label: test_label, **kwargs
+end
+
+def assert_stroke(*sources, **kwargs)
+  assert_draw 'noFill; stroke 0, 255, 0', *sources, label: test_label, **kwargs
+end
+
+def assert_fill_stroke(*sources, **kwargs)
+  assert_draw 'stroke 0, 255, 0', *sources, label: test_label, **kwargs
 end
