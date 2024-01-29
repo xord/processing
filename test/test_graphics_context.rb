6f58c20e71f93423c37b12af50ba10c96f511efd
xordog <xordog@gmail.com>
2024-01-29 23:48:51 +0900

    Fix Matrix::to_a order

--- test/test_graphics_context.rb
+++ test/test_graphics_context.rb
@@ -789,19 +789,19 @@ class TestGraphicsContext < Test::Unit::TestCase
 
     g.beginDraw do
       g.resetMatrix
-      g.applyMatrix 1, 2, 3, 4, 5, 6
-      assert_equal [1,2,0,0, 3,4,0,0, 0,0,1,0, 5,6,0,1], painterMatrix.call
-
-      g.resetMatrix
-      g.applyMatrix 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
-      assert_equal [1,5,9,13, 2,6,10,14, 3,7,11,15, 4,8,12,16], painterMatrix.call
+      g.applyMatrix  1, 2, 3, 4, 5, 6
+      assert_equal [1,3,0,5, 2,4,0,6, 0,0,1,0, 0,0,0,1], painterMatrix.call
 
       g.resetMatrix
       g.applyMatrix [1, 2, 3, 4, 5, 6]
-      assert_equal [1,2,0,0, 3,4,0,0, 0,0,1,0, 5,6,0,1], painterMatrix.call
+      assert_equal [1,3,0,5, 2,4,0,6, 0,0,1,0, 0,0,0,1], painterMatrix.call
 
       g.resetMatrix
-      g.applyMatrix [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+      g.applyMatrix  1,2,3,4, 5,6,7,8, 9,10,11,12, 13,14,15,16
+      assert_equal [1,5,9,13, 2,6,10,14, 3,7,11,15, 4,8,12,16], painterMatrix.call
+
+      g.resetMatrix
+      g.applyMatrix [1,2,3,4, 5,6,7,8, 9,10,11,12, 13,14,15,16]
       assert_equal [1,5,9,13, 2,6,10,14, 3,7,11,15, 4,8,12,16], painterMatrix.call
 
       ((0..17).to_a - [6, 16]).each do |n|
