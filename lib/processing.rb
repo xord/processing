090be7d5b1939abd8519e8fe282fa7b81c9199c0
xordog <xordog@gmail.com>
2025-01-24 03:49:56 +0900

    Event blocks can be defined as methods with 'def'

--- lib/processing.rb
+++ lib/processing.rb
@@ -2,10 +2,11 @@ require 'processing/all'
 
 
 module Processing
-  w = (ENV['WIDTH']  || 500).to_i
-  h = (ENV['HEIGHT'] || 500).to_i
+  w         = (ENV['WIDTH']  || 500).to_i
+  h         = (ENV['HEIGHT'] || 500).to_i
   WINDOW__  = Processing::Window.new(w, h) {start}
   CONTEXT__ = Processing::Context.new WINDOW__
+  BINDING__ = binding
 
   refine Object do
     (CONTEXT__.methods - Object.instance_methods)
@@ -15,6 +16,12 @@ module Processing
           CONTEXT__.__send__ method, *args, **kwargs, &block
         end
       end
+
+    def Object.method_added(method)
+      if Processing::Context::EVENT_NAMES__.include? method
+        CONTEXT__.__send__(method) {BINDING__.eval method.to_s}
+      end
+    end
   end
 end# Processing
 
